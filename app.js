6e88fa25f7c18ea0aac0464aac760aca71fa3d8c
AndriiHamasa <andrii.khamaza@nure.ua>
2023-10-04 18:02:50 +0300

    create server

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express"
+import fetch from "node-fetch"
+import "dotenv/config"
+import logger from "morgan";
+import cors from "cors";
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+// Обработка POST-запросов
+app.post('/send-message', async (req, res) => {
+  try {
+    console.log('req.body ==>> ', req.body )
+    const { name, number } = req.body;
+    const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+    const telegramChatId = process.env.TELEGRAM_CHAT_ID;
+
+    if (!name || !number) {
+      return res.status(400).json({ error: 'Name and number are required fields' });
+    }
+
+    // Отправляем сообщение в Telegram
+    const response = await fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ chat_id: telegramChatId, text: `Name: ${name}, Number: ${number}` }),
+    });
+
+    const data = await response.json();
+    if (data.ok) {
+      return res.json({ message: 'Message sent successfully' });
+    } else {
+      return res.status(500).json({ error: 'Failed to send message to Telegram' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
